444ec646c70a130f667bea5f683c14be8bf25d1a
kongsherry <546235973@qq.com>
2025-10-30 18:21:51 +0800

    Update trap.c

--- lab3/kern/trap/trap.c
+++ lab3/kern/trap/trap.c
@@ -179,6 +179,9 @@ void exception_handler(struct trapframe *tf) {
              *(2)输出异常指令地址
              *(3)更新 tf->epc寄存器
             */
+            cprintf("Exception type: Illegal instruction\n");
+            cprintf("Illegal instruction caught at 0x%08x\n", tf->epc);
+            tf->epc += 4;            
             break;
         case CAUSE_BREAKPOINT:
             //断点异常处理
@@ -187,6 +190,9 @@ void exception_handler(struct trapframe *tf) {
              *(2)输出异常指令地址
              *(3)更新 tf->epc寄存器
             */
+            cprintf("Exception type: Breakpoint\n");
+            cprintf("Breakpoint caught at 0x%08x\n", tf->epc);
+            tf->epc += 2;             
             break;
         case CAUSE_MISALIGNED_LOAD:
             break;
@@ -230,4 +236,5 @@ void trap(struct trapframe *tf) {
     // dispatch based on what type of trap occurred
     trap_dispatch(tf);
 }
+
 
